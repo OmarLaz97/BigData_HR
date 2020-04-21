49d93e03848513734c5a2fd5ea78f1a07298dbe0
WalaaIhab33 <38043912+WalaaIhab33@users.noreply.github.com>
2020-04-22 01:23:23 +0200

    Classifier

--- corrAndClassifier.R
+++ corrAndClassifier.R
@@ -1,5 +1,8 @@
+library(ROCR)
+
 # clean environment 
 rm(list=ls())
+
 
 data <- read.csv("employee_data.csv")
 
@@ -33,9 +36,34 @@ corMat <- cor(TempData[ , !(names(TempData) %in% c("Attrition"))])
 summary(corMat)
 
 trainingSet <- head(data, 3010)
-testSet <- tail(dfm, nrow(data)-3010)
+testSet <- tail(data, nrow(data)-3010)
 
 mylogit <- glm(Attrition ~.,
                data =trainingSet, family=binomial(link="logit"),
                na.action=na.pass)
+summary(mylogit) 
+
+
+pred = predict(mylogit, type="response") # this returns the probability scores on the training data
+predObj = prediction(pred, trainingSet$Attrition) # prediction object needed by ROCR
+
+rocObj = performance(predObj, measure="tpr", x.measure="fpr")  # creates ROC curve obj
+aucObj = performance(predObj, measure="auc")  # auc object
+
+auc = aucObj@y.values[[1]]
+auc   # the auc score: tells you how well the model predicts.
+
+plot(rocObj, main = paste("Area under the curve:", auc))
+
+newdata1 <- testSet[ , !(names(TempData) %in% c("Attrition"))]
+newdata1$Attrition <- predict (mylogit,newdata=newdata1,type="response")
+
+newdata1$Attrition <- factor(ifelse(newdata1$Attrition >= 0.50, 1, 0))
+
+confusionMatrix <- table( newdata1$Attrition, testSet$Attrition)
+confusionMatrix
+
+accuracy <- 100*(sum(diag(confusionMatrix))/sum(confusionMatrix))
+accuracy
+
 
