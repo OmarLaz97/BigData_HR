8e6f2741ea3f58066dc74364ce1fbc33d6e5f1ec
Maram Khattab <maram.khattab@hotmail.com>
2020-04-24 16:20:34 +0200

    association rules

--- Associations.R
+++ Associations.R
@@ -1,4 +1,23 @@
 rm(list=ls())
 library(arules)
+library(fastDummies)
+
 data <- read.csv("employee_data.csv")
-rules<-apriori(data, parameter = list(supp = 0.01,minlen=2, conf = 0.5, target = "rules"))
+
+# Categorical data to transform to n-1 variables
+CatArray <- c("Department", "EducationField", "Gender", "JobRole", "MaritalStatus")
+
+
+#categorize monthly income into 4 categories
+income<-summary(data$MonthlyIncome)
+data$MonthlyIncome<-cut(data$MonthlyIncome, c(10090,29260,65060,199990), labels = c("low","medium","high"))
+
+test<-data[c("MonthlyIncome","Gender","BusinessTravel","MaritalStatus")]
+names <- c(1:4)
+test[,names] <- lapply(test[,names] , factor)
+transactions<-as(test,"transactions")
+#inspect(transactions)
+rules<-apriori(transactions, parameter = list(supp = 0.01,minlen=4, conf = 0.5, target = "rules"))
+#inspect(rules)
+support<-head(sort(rules, by="support"),6)
+inspect(support)
