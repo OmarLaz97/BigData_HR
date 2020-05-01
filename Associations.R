ed9d4017eadf2ca42f5fc669ebd1889b9a169548
Maram Khattab <maram.khattab@hotmail.com>
2020-05-01 05:56:18 +0200

    3 scenarios

--- Associations.R
+++ Associations.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 library(arules)
 library(fastDummies)
+#library(arulesViz)
 
 data <- read.csv("employee_data.csv")
 
@@ -22,32 +23,42 @@ data$Age<-cut(data$Age, c(Age["Min."],Age["1st Qu."],Age["3rd Qu."],Age["Max."])
 
 #distance from home 
 Distance<-summary(data$DistanceFromHome)
-data$DistanceFromHome<-discretize(data$DistanceFromHome,method = "cluster")
+data$DistanceFromHome<-discretize(data$DistanceFromHome,breaks=3)
 
 #number of companies worked for
 Companies<-summary(data$NumCompaniesWorked)
-data$NumCompaniesWorked<-discretize(data$NumCompaniesWorked,method = "cluster")
+data$NumCompaniesWorked<-discretize(data$NumCompaniesWorked,breaks=3)
 
 #years at company
 yearsCompany<-summary(data$YearsAtCompany)
-data$YearsAtCompany<-discretize(data$YearsAtCompany,method = "cluster")
+data$YearsAtCompany<-discretize(data$YearsAtCompany,breaks=3)
 
 #years with current manager
 yearsManager<-summary(data$YearsWithCurrManager)
-data$YearsWithCurrManager<-discretize(data$YearsWithCurrManager,method = "cluster")
+data$YearsWithCurrManager<-discretize(data$YearsWithCurrManager,breaks=3)
+
+# inspecting rules for work life balance <3
+
+assocData<-data[data[, "WorkLifeBalance"] < 3,]
+assocData<-assocData[,c("DistanceFromHome","MaritalStatus","AverageWorkingHours","WorkLifeBalance","MonthlyIncome")]
 
 
-assocData<-data
-assocData[,c("EmployeeID","YearsWithCurrManager","PercentSalaryHike","StockOptionLevel","Department")]<-list(NULL)
+#attrition with the most correlated variables
 
+#assocData<-data[,c("MaritalStatus","AverageWorkingHours","Age","TotalWorkingYears","Attrition")]
 
-names <- c(1:22)
+#Job satisfaction >=3
+if (FALSE){
+  assocData<-data[data[,"JobSatisfaction"]>=3,]
+  assocData<-assocData[,c("JobRole","Department","JobSatisfaction","YearsWithCurrManager","BusinessTravel","MonthlyIncome")]
+  
+}
+
+names <- c(1:5)
 assocData[,names] <- lapply(assocData[,names] , factor)
 transactions<-as(assocData,"transactions")
-#inspect(transactions)
-rules<-apriori(transactions, parameter = list(supp = 0.05,minlen=3, conf = 0.5, target = "rules"))
-#inspect(rules)
-support<-head(sort(rules, by="support"),6)
-#inspect(support)
-lift<-head(sort(rules,by="lift"),6)
+rules<-apriori(transactions, parameter = list(supp = 0.01,minlen=3, conf = 0.01, target = "rules",maxtime=0))
+rules_subset <- subset(rules, (rhs %in% paste0("WorkLifeBalance=", unique(assocData$WorkLifeBalance))))
+lift<-head(sort(rules_subset,by="lift"),6)
 inspect(lift)
+#plot(rules_subset, measure=c("support","confidence"),shading="lift")
